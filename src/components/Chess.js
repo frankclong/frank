942ea0dea7c141fd063bc8461158976ab6027122
Frank Long <fclong@uwaterloo.ca>
2023-12-31 14:29:45 -0500

    Chess page UI and clean up

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 const token = 'lip_5muroQrpPxYzLwhRs59J';
 
+const LICHESS_TIME_CONTROL_MAP = {
+    "Rapid" : "rapid",
+    "Blitz" : "blitz",
+    "Bullet" : "bullet"
+}
+
+const TIME_CONTROLS = ["Bullet", "Blitz", "Rapid"]
+
 export default function Chess() {
     const [data, setData] = useState(null);
 
@@ -20,28 +28,28 @@ export default function Chess() {
     return (
         <section id="chess">
             <div className="container px-5 py-10 mx-auto text-center lg:px-40">
-                <div className="flex flex-col w-full mb-20">
                 <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
                     Chess
                 </h1>
+                <div className="flex items-center justify-center mb-2">
+                    <img src="./lichess.png" alt="Your Image" className="w-10 h-10 rounded-full" />
+                    <h2 className="text-lg font-semibold ml-4">Lichess</h2>
                 </div>
-                <div className="flex flex-wrap -m-4">
-                    <div>
-                        <h2>Username:</h2>
-                        <p>{data ? data['username'] : "Loading..."}</p>
-                    </div>
-                    <div>
-                        <h2>Bullet</h2>
-                        <p>{data ? data['perfs']['bullet']['rating'] : "Loading..."}</p>
-                    </div>
-                    <div>
-                        <h2>Blitz</h2>
-                        <p>{data ? data['perfs']['blitz']['rating'] : "Loading..."}</p>
-                    </div>
-                    <div>
-                        <h2>Rapid</h2>
-                        <p>{data ? data['perfs']['rapid']['rating'] : "Loading..."}</p>
-                    </div>
+
+                <p class="text-xl font-bold mb-4">
+                    <a href="https://lichess.org/@/dragonsp" class="text-white-500">
+                        {data ? data['username'] : "Loading..."}
+                        </a>
+                </p>
+                <div class="flex mb-2">
+                    {
+                        TIME_CONTROLS.map(timeControl => (
+                            <div class="flex-shrink-0 mr-4">
+                                <span class="font-semibold">{timeControl}</span>
+                                <span class="ml-2">{data ? data['perfs'][LICHESS_TIME_CONTROL_MAP[timeControl]]['rating'] : "Loading..."}</span>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
