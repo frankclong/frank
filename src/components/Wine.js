c79b419b1fd42afd0d9fde2a461710a39f34d249
Frank Long <fclong@uwaterloo.ca>
2024-01-03 21:33:21 -0500

    Wine section files

---
+++ src/components/Wine.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import { wines } from "../wineData";
+
+export default function Wine() {
+    return (
+        <section id="wine">
+            
+        </section>
+    )
+}
---
+++ src/wineData.js
@@ -0,0 +1,47 @@
+wines = [
+    {
+        "image" : "",
+        "name" : "Torbreck Woodcutter's Shiraz",
+        "vintage" : 2021,
+        "notes" : "This was the first wine I tried after becoming fascinated about the world of wine. I was seriously (keyword) trying to experience it - smell, taste, sight, touch. The smell was very nice and the taste even better. There was the hint of peppery taste often found in Shiraz and overall the balance was excellent. A full bodied wine. Reminded me of sunset over a field that was drying out. Huge fan - definitely got me very excited for the world of wine before me",
+        "variety" : "Shiraz",
+        "region" : "Barossa Valley, Australia",
+        "priceCAD" : 29.95
+    },
+    {
+        "image" : "",
+        "name" : "Campo Viejo Rioja Tempranillo",
+        "vintage" : 2021,
+        "notes" : "Purchased to enjoy with a chorizo fried rice dish, but did not have a cork screw on hand. Attempted to uncork by screwing a screw in and pulling with hammer but wasn't too effective and I didn't want to risk anything. Eventually acquired a corkscrew to enjoy this wine. Overall, very similar to the Torbreck except lighter. Still enjoyable but definitely not as complex and forthcoming. Was not able to finish the bottle as degradation ruined it. Also the cork was damaged (side that was in the wine) and bits were falling in. Given it was almost half the price of the Torbreck, it was still a good experience and now I know Tempranillo is pretty good",
+        "variety" : "Tempranillo",
+        "region" : "Rioja, Spain",
+        "priceCAD" : 16.45
+    },
+    {
+        "image" : "",
+        "name" : "Domaine d'Argenson Les Pierreux Brouilly",
+        "vintage" : 2018,
+        "notes" : "Pretty light overall, tasted some minerals - kinda like pig blood curd (?). The sensations did not linger on the tongue. It was definitely questionable on the first day, but subsequent days of drinking were quite enjoyable but the mineral taste was gone. Reminded me of living rocks, a plant recently purchased for a friend",
+        "variety" : "Gamay",
+        "region" : "Beaujolais, France",
+        "priceCAD" : 22.95
+    },
+    {
+        "image" : "",
+        "name" : "Carnivor Zinfandel",
+        "vintage" : 2021,
+        "notes" : "First thing I noticed was the sweetness (9g/L). The balance was a bit off but it was a very sippable wine. Next few days felt like the balance was improving and it became very enjoyable. Fruity and sweet, couldn't get enough!",
+        "variety" : "Zinfandel",
+        "region" : "Lodi, California, USA",
+        "price" : 18.95
+    },
+    {
+        "image" : "",
+        "name" : "Catena Malbec",
+        "vintage" : 2021,
+        "notes" : "",
+        "variety" : "Malbec",
+        "region" : "Mendoza, Argentina",
+        "price" : 23.95
+    }
+]
